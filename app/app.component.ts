20f41036341456739d9a44e66b643e1832e1bce2
Aitor Ibañez <freelance@aitoribanez.com>
2016-10-08 21:52:02 +0200

    transpiled files to dist

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,13 +3,52 @@ import { Component } from '@angular/core';
 @Component ({
   selector: 'my-app',
   template:  `
-    <div class="jumbotron">
-      <h1>Welcome to Our App!</h1>
-    </div>
+    <header>
+      <nav class="navbar navbar-inverse">
+        <div class="navbar-header">
+          <a href="/" class="navbar-brand">Angular2 App!</a>
+        </div>
+      </nav>
+    </header>
+
+    <main>
+      <div class="row">
+        <div class="col-sm-4">
+
+          <div *ngIf="users">
+            <ul class="list-group user-list">
+              <li class="list-group-item"
+                *ngFor="let user of users">
+                {{ user.name}} ({{ user.username }})
+              </li>
+            </ul>
+          </div>
+
+        </div>
+
+        <div class="col-sm-8">
+          <div class="jumbotron">
+            <h1>Welcome to Our App!</h1>
+            <p>{{ message }}</p>
+          </div>
+        </div>
+      </div> <!-- row -->
+    </main>
+
+    <footer class="text-center">
+      Copyright &copy; 2016
+    </footer>
   `,
   styles: [`
     .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }
   `]
 })
 
-export class AppComponent {}
+export class AppComponent {
+  message = 'Hello';
+  users = [
+    { id: 25, name: 'Chris', username: 'chris' },
+    { id:26, name: 'Nick', username: 'twitnick' },
+    { id:27, name: 'Holly', username: 'hollynick' }
+  ];
+}
