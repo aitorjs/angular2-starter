dbda9c1b9127b8829b5dbd356ad23bc83b95afd4
Aitor Ibañez <freelance@aitoribanez.com>
2016-10-09 02:38:57 +0200

    improving example components app

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,54 +1,24 @@
 import { Component } from '@angular/core';
+import { User } from './shared/models/user'
 
 @Component ({
   selector: 'my-app',
-  template:  `
-    <header>
-      <nav class="navbar navbar-inverse">
-        <div class="navbar-header">
-          <a href="/" class="navbar-brand">Angular2 App!</a>
-        </div>
-      </nav>
-    </header>
-
-    <main>
-      <div class="row">
-        <div class="col-sm-4">
-
-          <div *ngIf="users">
-            <ul class="list-group user-list">
-              <li class="list-group-item"
-                *ngFor="let user of users">
-                {{ user.name}} ({{ user.username }})
-              </li>
-            </ul>
-          </div>
-
-        </div>
-
-        <div class="col-sm-8">
-          <div class="jumbotron">
-            <h1>Welcome to Our App!</h1>
-            <p>{{ message }}</p>
-          </div>
-        </div>
-      </div> <!-- row -->
-    </main>
-
-    <footer class="text-center">
-      Copyright &copy; 2016
-    </footer>
-  `,
-  styles: [`
-    .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }
-  `]
+  templateUrl: './app/app.component.html',
+  styles: ['./app/app.component.css']
 })
 
 export class AppComponent {
-  message = 'Hello';
-  users = [
+  message: string = 'Hello';
+  users: User[] = [
     { id: 25, name: 'Chris', username: 'chris' },
     { id:26, name: 'Nick', username: 'twitnick' },
     { id:27, name: 'Holly', username: 'hollynick' }
   ];
+  activeUser: User;
+
+  selectUser(user) {
+    this.activeUser = user;
+    console.log(this.activeUser);
+    
+  }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,19 @@
 // Para encapsular el uso de componentes en un sitio
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { UserProfileComponent } from './users/user-profile.component';
 
 @NgModule({
-    imports: [ BrowserModule ],
-    declarations: [ AppComponent ],
+    imports: [ 
+      BrowserModule,
+      FormsModule
+    ],
+    declarations: [
+      AppComponent,
+      UserProfileComponent
+    ],
     bootstrap: [ AppComponent ]
 })
 
